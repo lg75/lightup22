21d045d497e2de1ca4bf31a1c1bafb5cc7bc2b61
Boris Hruruguen <boris.huruguen@gmail.com>
2015-09-05 11:34:31 +0200

    model Chapiter (+crash test ok)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150905084244) do
+ActiveRecord::Schema.define(version: 20150905093122) do
+
+  create_table "chapters", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "chapters", ["course_id"], name: "index_chapters_on_course_id"
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
